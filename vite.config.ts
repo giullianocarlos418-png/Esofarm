e8882dc8771a02ed0e1dbd11c416c58f31903309
giullianocarlos418-png <giullianocarlos418@gmail.com>
2026-05-14 00:43:55 -0300

    fix merge markers

--- vite.config.ts
+++ vite.config.ts
@@ -10,30 +10,30 @@ function figmaAssetResolver() {
     resolveId(id) {
       if (id.startsWith('figma:asset/')) {
         const filename = id.replace('figma:asset/', '')
-        return path.resolve(__dirname, 'src/assets', filename)
+        return path.resolve('__dirname', 'src/assets', filename)
       }
     },
   }
 }
 
 export default defineConfig({
+  base: '/Esofarm/',
+
   plugins: [
     figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
-      // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve('__dirname', './src'),
     },
   },
 
-<<<<<<< HEAD
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
-=======
->>>>>>> 5f872cbafcde0e094c246d5352a507a01c7aa315
+  build: {
+    outDir: 'dist',
+  },
+
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
